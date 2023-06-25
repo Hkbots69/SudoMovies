ab9a02ff1813e7855e40a086d268e2a9a402766e
MrHadik <chavdah80@gmail.com>
2023-06-25 13:11:25 +0530

    Trying to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const Dotenv = require('dotenv-webpack');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
-  // ...other webpack configuration options
-
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify")
+    }
+  },
   plugins: [
-    new Dotenv()
+    new Dotenv(),
+    new NodePolyfillPlugin()
   ],
 };
